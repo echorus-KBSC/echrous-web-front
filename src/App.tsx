f3216b5dff253609a9d89d1dc2fec8e8147a3072
kasterra <kasterra@naver.com>
2022-07-04 15:12:39 +0900

    dev : basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import { css, Global } from '@emotion/react';
+import normalize from 'emotion-normalize';
+import { theme as chakraTheme } from '@chakra-ui/react';
+import Routes from './pages/Routes';
+const App = () => {
+    return (
+        <>
+            <Global
+                styles={css`
+                    ${normalize}
+                    * {
+                        box-sizing: border-box;
+                        background-color: transparent;
+                        border: none;
+                        font: ${chakraTheme.fonts.body};
+                    }
+                    a {
+                        text-decoration: none;
+                        color: inherit;
+                    }
+                    button:not([disabled]) {
+                        cursor: pointer;
+                    }
+                    body {
+                        background-color: #f5f5f5;
+                    }
+                    h1,
+                    h2,
+                    h3,
+                    h4,
+                    h5,
+                    h6 {
+                        margin: 0;
+                        font: ${chakraTheme.fonts.heading};
+                    }
+                `}
+            />
+            <Routes />
+        </>
+    );
+};
 
 export default App;
